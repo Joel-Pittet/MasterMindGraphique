a87e6af5adcb143426a4ffba4613548c2f3c2165
Joël Pittet <ph06ilj@eduvaud.ch>
2023-11-30 16:34:51 +0100

    labels et boutons dynamiques

--- P_MasterMind-Graphique/Game.Designer.cs
+++ P_MasterMind-Graphique/Game.Designer.cs
@@ -31,17 +31,6 @@
             this.lblTries = new System.Windows.Forms.Label();
             this.lblresultUserTry = new System.Windows.Forms.Label();
             this.lblPossibleColors = new System.Windows.Forms.Label();
-            this.lblColor1 = new System.Windows.Forms.Label();
-            this.lblColor2 = new System.Windows.Forms.Label();
-            this.lblColor3 = new System.Windows.Forms.Label();
-            this.lblColor4 = new System.Windows.Forms.Label();
-            this.btnRed = new System.Windows.Forms.Button();
-            this.btnGreen = new System.Windows.Forms.Button();
-            this.btnBlue = new System.Windows.Forms.Button();
-            this.btnOrange = new System.Windows.Forms.Button();
-            this.btnPink = new System.Windows.Forms.Button();
-            this.btnPurple = new System.Windows.Forms.Button();
-            this.btnYellow = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
@@ -74,120 +63,6 @@
             this.lblPossibleColors.TabIndex = 0;
             this.lblPossibleColors.Text = "Couleurs disponibles";
             // 
-            // lblColor1
-            // 
-            this.lblColor1.AutoSize = true;
-            this.lblColor1.BackColor = System.Drawing.Color.White;
-            this.lblColor1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.lblColor1.Location = new System.Drawing.Point(12, 36);
-            this.lblColor1.Name = "lblColor1";
-            this.lblColor1.Size = new System.Drawing.Size(15, 15);
-            this.lblColor1.TabIndex = 1;
-            this.lblColor1.Text = "  ";
-            // 
-            // lblColor2
-            // 
-            this.lblColor2.AutoSize = true;
-            this.lblColor2.BackColor = System.Drawing.Color.White;
-            this.lblColor2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.lblColor2.Location = new System.Drawing.Point(33, 36);
-            this.lblColor2.Name = "lblColor2";
-            this.lblColor2.Size = new System.Drawing.Size(15, 15);
-            this.lblColor2.TabIndex = 1;
-            this.lblColor2.Text = "  ";
-            // 
-            // lblColor3
-            // 
-            this.lblColor3.AutoSize = true;
-            this.lblColor3.BackColor = System.Drawing.Color.White;
-            this.lblColor3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.lblColor3.Location = new System.Drawing.Point(54, 36);
-            this.lblColor3.Name = "lblColor3";
-            this.lblColor3.Size = new System.Drawing.Size(15, 15);
-            this.lblColor3.TabIndex = 1;
-            this.lblColor3.Text = "  ";
-            // 
-            // lblColor4
-            // 
-            this.lblColor4.AutoSize = true;
-            this.lblColor4.BackColor = System.Drawing.Color.White;
-            this.lblColor4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.lblColor4.Location = new System.Drawing.Point(75, 36);
-            this.lblColor4.Name = "lblColor4";
-            this.lblColor4.Size = new System.Drawing.Size(15, 15);
-            this.lblColor4.TabIndex = 1;
-            this.lblColor4.Text = "  ";
-            // 
-            // btnRed
-            // 
-            this.btnRed.BackColor = System.Drawing.Color.Red;
-            this.btnRed.Location = new System.Drawing.Point(260, 31);
-            this.btnRed.Name = "btnRed";
-            this.btnRed.Size = new System.Drawing.Size(24, 23);
-            this.btnRed.TabIndex = 2;
-            this.btnRed.UseVisualStyleBackColor = false;
-            this.btnRed.Click += new System.EventHandler(this.colors_Click);
-            // 
-            // btnGreen
-            // 
-            this.btnGreen.BackColor = System.Drawing.Color.Lime;
-            this.btnGreen.Location = new System.Drawing.Point(290, 31);
-            this.btnGreen.Name = "btnGreen";
-            this.btnGreen.Size = new System.Drawing.Size(24, 23);
-            this.btnGreen.TabIndex = 2;
-            this.btnGreen.UseVisualStyleBackColor = false;
-            this.btnGreen.Click += new System.EventHandler(this.colors_Click);
-            // 
-            // btnBlue
-            // 
-            this.btnBlue.BackColor = System.Drawing.Color.Blue;
-            this.btnBlue.Location = new System.Drawing.Point(320, 31);
-            this.btnBlue.Name = "btnBlue";
-            this.btnBlue.Size = new System.Drawing.Size(24, 23);
-            this.btnBlue.TabIndex = 2;
-            this.btnBlue.UseVisualStyleBackColor = false;
-            this.btnBlue.Click += new System.EventHandler(this.colors_Click);
-            // 
-            // btnOrange
-            // 
-            this.btnOrange.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.btnOrange.Location = new System.Drawing.Point(350, 31);
-            this.btnOrange.Name = "btnOrange";
-            this.btnOrange.Size = new System.Drawing.Size(24, 23);
-            this.btnOrange.TabIndex = 2;
-            this.btnOrange.UseVisualStyleBackColor = false;
-            this.btnOrange.Click += new System.EventHandler(this.colors_Click);
-            // 
-            // btnPink
-            // 
-            this.btnPink.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.btnPink.Location = new System.Drawing.Point(333, 65);
-            this.btnPink.Name = "btnPink";
-            this.btnPink.Size = new System.Drawing.Size(24, 23);
-            this.btnPink.TabIndex = 2;
-            this.btnPink.UseVisualStyleBackColor = false;
-            this.btnPink.Click += new System.EventHandler(this.colors_Click);
-            // 
-            // btnPurple
-            // 
-            this.btnPurple.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.btnPurple.Location = new System.Drawing.Point(303, 65);
-            this.btnPurple.Name = "btnPurple";
-            this.btnPurple.Size = new System.Drawing.Size(24, 23);
-            this.btnPurple.TabIndex = 2;
-            this.btnPurple.UseVisualStyleBackColor = false;
-            this.btnPurple.Click += new System.EventHandler(this.colors_Click);
-            // 
-            // btnYellow
-            // 
-            this.btnYellow.BackColor = System.Drawing.Color.Yellow;
-            this.btnYellow.Location = new System.Drawing.Point(273, 65);
-            this.btnYellow.Name = "btnYellow";
-            this.btnYellow.Size = new System.Drawing.Size(24, 23);
-            this.btnYellow.TabIndex = 2;
-            this.btnYellow.UseVisualStyleBackColor = false;
-            this.btnYellow.Click += new System.EventHandler(this.colors_Click);
-            // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(273, 126);
@@ -196,7 +71,6 @@
             this.button1.TabIndex = 3;
             this.button1.Text = "Effacer";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.btnDeleteTry_Click);
             // 
             // button2
             // 
@@ -215,22 +89,12 @@
             this.ClientSize = new System.Drawing.Size(399, 640);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.btnYellow);
-            this.Controls.Add(this.btnPurple);
-            this.Controls.Add(this.btnPink);
-            this.Controls.Add(this.btnOrange);
-            this.Controls.Add(this.btnBlue);
-            this.Controls.Add(this.btnGreen);
-            this.Controls.Add(this.btnRed);
-            this.Controls.Add(this.lblColor4);
-            this.Controls.Add(this.lblColor3);
-            this.Controls.Add(this.lblColor2);
-            this.Controls.Add(this.lblColor1);
             this.Controls.Add(this.lblPossibleColors);
             this.Controls.Add(this.lblresultUserTry);
             this.Controls.Add(this.lblTries);
             this.Name = "Game";
             this.Text = "Game";
+            this.Load += new System.EventHandler(this.Game_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -241,17 +105,6 @@
         private System.Windows.Forms.Label lblTries;
         private System.Windows.Forms.Label lblresultUserTry;
         private System.Windows.Forms.Label lblPossibleColors;
-        private System.Windows.Forms.Label lblColor1;
-        private System.Windows.Forms.Label lblColor2;
-        private System.Windows.Forms.Label lblColor3;
-        private System.Windows.Forms.Label lblColor4;
-        private System.Windows.Forms.Button btnRed;
-        private System.Windows.Forms.Button btnGreen;
-        private System.Windows.Forms.Button btnBlue;
-        private System.Windows.Forms.Button btnOrange;
-        private System.Windows.Forms.Button btnPink;
-        private System.Windows.Forms.Button btnPurple;
-        private System.Windows.Forms.Button btnYellow;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
     }
--- P_MasterMind-Graphique/Game.cs
+++ P_MasterMind-Graphique/Game.cs
@@ -18,36 +18,67 @@ namespace P_MasterMind_Graphique
         }
 
         //compteur pour placer les couleurs dans le bon label
-        int countLabel = 1;
+        //int countLabel = 1;
 
         //Liste pour stocker les couleurs disponibles
-        List<string> ColorList = new List<string>();
+        List<Color> ColorList = new List<Color>() 
+        {Color.Red, Color.Green, Color.Blue, Color.Orange, Color.Yellow, Color.Purple, Color.Pink};
 
         //Liste pour stocker le code secret
-        List<string> secretCode = new List<string>();
+        List<Color> secretCode = new List<Color>();
 
         //maximum de couleurs dans le code secret et dans les essais
         const int NB_COLORS = 4;
 
+        //nombre maximum d'essais
+        const int MAX_TRIES = 10;
+
         //Liste pour récuperer le code secret
         List<string> returnedRandomCode = new List<string>();
 
+        //tableau pour stocker les labels (essais du joueur)
+        Label[,] gridLabels = new Label[NB_COLORS, MAX_TRIES];
 
+        //Compteur pour les labels d'essais
+        int lblCount = 0;
+
+        //Compteur pour le nombre de label par ligne
+        int nbLabelX = 0;
+
+        //Pour placement du panel de la zone d'essais
+        int panelTriesAxisX = 12;
+        int panelTriesAxisY = 36;
+
+        //Taille par défaut des labels
+        const int LBL_SIZE = 25;
+
+        //Pour placement du panel de la zone des couleurs
+        int panelColorsAxisX = 130;
+        int panelColorsAxisY = 300;
+
+        //Taille par défaut des boutons de couleurs
+        const int BTN_COLOR_SIZE = 25;
+
+        //Pour placement des boutons des couleurs
+        int btnColorAxisX = 0;
+        int btnColorAxisY = 0;
+
+        //Pour espacer les boutons de couleurs
+        const int SPACE_BETWEEN_BUTTON = 2;
         /// <summary>
         /// Crée le code secret
         /// </summary>
         /// <returns></returns>
-        List<string> RandomCode()
+        List<Color> RandomCode()
         {
-
             //Ajout des couleurs dans la liste
-            ColorList.Add("rouge");
-            ColorList.Add("vert");
-            ColorList.Add("bleu");
-            ColorList.Add("orange");
-            ColorList.Add("jaune");
-            ColorList.Add("violet");
-            ColorList.Add("rose");
+            ColorList.Add(Color.Red);
+            ColorList.Add(Color.Green);
+            ColorList.Add(Color.Blue);
+            ColorList.Add(Color.Orange);
+            ColorList.Add(Color.Yellow);
+            ColorList.Add(Color.Purple);
+            ColorList.Add(Color.Pink);
 
             //création du code secret en utilisant la liste des couleurs
             for (int i = 0; i < NB_COLORS; i++)
@@ -67,7 +98,7 @@ namespace P_MasterMind_Graphique
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void colors_Click(object sender, EventArgs e)
+        /*private void colors_Click(object sender, EventArgs e)
         {
             Button clickedButton = (Button)sender;
 
@@ -94,24 +125,7 @@ namespace P_MasterMind_Graphique
                     break;
             }
 
-        }
-
-        /// <summary>
-        /// Efface l'essai en cours
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void btnDeleteTry_Click(object sender, EventArgs e)
-        {
-            //Remet la couleur de base dans les labels
-            lblColor1.BackColor = Color.White;
-            lblColor2.BackColor = Color.White;
-            lblColor3.BackColor = Color.White;
-            lblColor4.BackColor = Color.White;
-
-            //Réinitialise le compteur de couleurs placée à 1
-            countLabel = 1;
-        }
+        }*/
 
         /// <summary>
         /// Quitte l'application
@@ -120,7 +134,161 @@ namespace P_MasterMind_Graphique
         /// <param name="e"></param>
         private void btnExitGame_Click(object sender, EventArgs e)
         {
+            //Quitte l'application
             Application.Exit();
+        }
+        /// <summary>
+        /// Lance la form en initialisant les panels d'essais et de couleurs
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Game_Load(object sender, EventArgs e)
+        {
+
+            /***********************************Panel des labels d'essais***************************************/
+
+            //Création du panel pour la zone d'essais
+            Panel pnlTries = new Panel();
+
+            //Placement du panel dans la Form
+            pnlTries.Location = new Point(panelTriesAxisX, panelTriesAxisY);
+
+            //Initialisation de la hauteur du panel
+            pnlTries.Height = MAX_TRIES * LBL_SIZE;
+
+            //Initialisation de la largeur du panel
+            pnlTries.Width = NB_COLORS * LBL_SIZE;
+
+            //Donne une bordure au panel
+            pnlTries.BorderStyle = BorderStyle.FixedSingle;
+
+            //Associe le panel à la form
+            Controls.Add(pnlTries);
+
+
+            /***************************************Panel pour zone de couleurs*********************************/
+
+            //Création du panel pour la zone des couleurs
+            Panel pnlColors = new Panel();
+
+            //Placement du panel de la zone des couleurs dans la Form
+            pnlColors.Location = new Point(panelColorsAxisX, panelColorsAxisY);
+
+            //Initialisation de la hauteur du panel de la zone des couleurs
+            pnlColors.Height = BTN_COLOR_SIZE + 3;
+
+            //Initialisation de la largeur du panel de la zone des couleurs
+            pnlColors.Width = (ColorList.Count + 1) * BTN_COLOR_SIZE;
+
+            //Donne une bordure au panel de la zone des couleurs
+            pnlColors.BorderStyle = BorderStyle.FixedSingle;
+
+            //Associe le panel à la form
+            Controls.Add(pnlColors);
+
+            /*********************************Affichage des labels de la zone d'essais***************************/
+
+
+            //Affiche les labels en colonne
+            for (int i = 0; i < MAX_TRIES; i++)
+            {
+
+                //change de ligne pour afficher les prochains labels en dessous de la ligne précédente
+                panelTriesAxisY =+ i * LBL_SIZE;
+
+                //réinitialise l'axe X pour que le bouton soit tout à gauche du panel
+                panelTriesAxisX = 0;
+
+                //Affiche les labels en ligne
+                for (int j = 0; j < NB_COLORS; j++)
+                {
+                    //Crée un label
+                    Label colorLabels = new Label();
+
+                    //Ajoute le label au tableau
+                    gridLabels[j, i] = colorLabels;
+
+                    //donne la couleur de fond blanche au label
+                    colorLabels.BackColor = Color.White;
+
+                    //Donne une bordure au label
+                    colorLabels.BorderStyle = BorderStyle.FixedSingle;
+
+                    //Ajoute 1 au compteur des labels pour le nom du label
+                    lblCount++;
+
+                    //Change le nom du label
+                    colorLabels.Name = "lblColor" + lblCount;
+
+                    //Change la taille du labe¨l
+                    colorLabels.Size = new Size(LBL_SIZE, LBL_SIZE);
+
+                    //Change le texte du label
+                    //colorLabels.Text = "";
+
+                    //Si la ligne est pleine, remet le compteur à zéro pour la ligne suivante
+                    if (nbLabelX == NB_COLORS)
+                    {
+                        //Reinitialise le compteur de case pour compter la ligne suivante
+                        nbLabelX = 0;
+                    }
+
+                    //Place le premier label de chaque ligne à gauche du panel sinon le place à la suite des autres
+                    if (nbLabelX == 0)
+                    {
+                        //Place les 1er labels sur le côté gauche du panel
+                        colorLabels.Location = new Point(panelTriesAxisX, panelTriesAxisY);
+                    }
+                    else
+                    {
+                        //Place les autres label sur la ligne les 1 après les autres
+                        colorLabels.Location = new Point(panelTriesAxisX += LBL_SIZE, panelTriesAxisY);
+                    }
+
+                    //Incrémente le compteur de labels par ligne
+                    nbLabelX++;
+
+                    //Association du label au panel
+                    pnlTries.Controls.Add(colorLabels);
+
+                }
+            }
+
+            /*********************************Affichage des boutons de la zone des couleurs***************************/
+
+            //Affiche chaque couleur disponible dans le jeu sous forme de boutons
+            for (int i = 0; i < ColorList.Count; i++)
+            {
+
+                //Crée un bouton pour chaque couleur
+                Button btnColor = new Button();
+
+                //Affiche la couleur dans le boutons
+                btnColor.BackColor = ColorList[i];
+
+                if(i == 0)
+                {
+                    //Affiche le bouton tout à gauche dans le panel
+                    btnColor.Location = new Point(btnColorAxisX, btnColorAxisY);
+                }
+                else
+                {
+                    //Affiche le bouton dans le panel
+                    btnColor.Location = new Point(btnColorAxisX + i*SPACE_BETWEEN_BUTTON + i*BTN_COLOR_SIZE, btnColorAxisY);
+                }
+                
+
+                //Change la taille du bouton
+                btnColor.Size = new Size(BTN_COLOR_SIZE, BTN_COLOR_SIZE + 1);
+
+                //Change le nom de boutons
+                btnColor.Name = "btn" + ColorList[i];
+
+                //Associe les boutons au panel des couleurs
+                pnlColors.Controls.Add(btnColor);
+
+
+            }
         }
     }
 }
